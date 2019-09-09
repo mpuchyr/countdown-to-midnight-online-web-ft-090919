322029a9624d034f645033455da5bcff1883203a
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-09 21:32:38 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  
+  until num == 0 do
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+  "HAPPY NEW YEAR!"
+  
+end
+
+def countdown_with_sleep(num)
+  
+  until num == 0 do
+    puts "#{num} SECONDS(S)!"
+    num -= 1
+    sleep 1
+  end
 end
